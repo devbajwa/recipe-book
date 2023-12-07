d480ee7a8f094c8fe20edc700d9f94ea4023ed48
wbajwa313 <wbajwa@choice.com.au>
2023-12-07 13:27:04 +1100

    Render card on View

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,5 +1,29 @@
+<script setup>
+import RecipeCard from '../components/RecipeCard.vue'
+</script>
 <template>
     <section>
         <h1>Recipes</h1>
+        <div class="card-container">
+            <RecipeCard />
+        <RecipeCard />
+        <RecipeCard />
+        </div>
+
     </section>
 </template>
+
+<style scoped>
+.card-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+/* Media query for smaller screens */
+@media screen and (max-width: 768px) {
+  .card-container{
+    display: block;
+  }
+}
+</style>
