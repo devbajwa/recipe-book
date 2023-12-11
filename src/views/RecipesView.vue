ba12d0ccbe3efcf3db4e15c1985eff908ecc7e4d
wbajwa313 <wbajwa@choice.com.au>
2023-12-11 14:58:09 +1100

    Format change

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,98 +1,106 @@
 <script setup>
-import {ref} from 'vue';
-import RecipeCard from '../components/RecipeCard.vue';
-
+import { ref } from "vue";
+import RecipeCard from "../components/RecipeCard.vue";
 
 const recipes = ref([
-    {
-        id: 1,
-        //date: getDate(),
-        name: "Biryani",
-        desc: "This is the delicious meal.",
-        isFav: true,
-        ingredients: [{ name: "Love", qty: "1sp" }, { name: "Effection",  qty: "2sp"}],
-        method: [{step: "Hold the hands"}, {step: "Never let go"}],
-        category: "Cuddle",
-        prepTime: "2 min",
-        cookTime: "Forever",
-        spiceLevel: "Low",
-        chef: "Wife",
-        occasion: "Life"
-    },
-    {
-        id: 2,
-        //date: getDate(),
-        name: "Nihari",
-        desc: "This is the delicious meal.",
-        isFav: false,
-        ingredients: [{ name: "Love", qty: "1sp" }, { name: "Effection",  qty: "2sp"}],
-        method: [{step: "Hold the hands"}, {step: "Never let go"}],
-        category: "Cuddle",
-        prepTime: "2 min",
-        cookTime: "Forever",
-        spiceLevel: "Medium",
-        chef: "Wife",
-        occasion: "Life"
-    },
-    {
-        id: 3,
-        //date: getDate(),
-        name: "Samosa",
-        desc: "This is the delicious meal. This is the delicious meal. This is the delicious meal. This is the delicious meal.",
-        isFav: true,
-        ingredients: [{ name: "Love", qty: "1sp" }, { name: "Effection",  qty: "2sp"}],
-        method: [{step: "Hold the hands"}, {step: "Never let go"}],
-        category: "Cuddle",
-        prepTime: "2 min",
-        cookTime: "Forever",
-        spiceLevel: "Extreme",
-        chef: "Wife",
-        occasion: "Life"
-    },
-    {
-        id: 4,
-        //date: getDate(),
-        name: "Haleem",
-        desc: "This is the delicious meal.",
-        isFav: true,
-        ingredients: [{ name: "Love", qty: "1sp" }, { name: "Effection",  qty: "2sp"}],
-        method: [{step: "Hold the hands"}, {step: "Never let go"}],
-        category: "Cuddle",
-        prepTime: "2 min",
-        cookTime: "1 hr",
-        spiceLevel: "High",
-        chef: "Wife",
-        occasion: "Life"
-    }
-])
-
-//console.log(JSON.stringify(recipes.value))
+  {
+    id: 1,
+    //date: getDate(),
+    name: "Biryani",
+    desc: "This is the delicious meal.",
+    isFav: true,
+    ingredients: [
+      { name: "Love", qty: "1sp" },
+      { name: "Effection", qty: "2sp" },
+    ],
+    method: [{ step: "Hold the hands" }, { step: "Never let go" }],
+    category: "Cuddle",
+    prepTime: "2 min",
+    cookTime: "Forever",
+    spiceLevel: "Low",
+    chef: "Wife",
+    occasion: "Life",
+  },
+  {
+    id: 2,
+    //date: getDate(),
+    name: "Nihari",
+    desc: "This is the delicious meal.",
+    isFav: false,
+    ingredients: [
+      { name: "Love", qty: "1sp" },
+      { name: "Effection", qty: "2sp" },
+    ],
+    method: [{ step: "Hold the hands" }, { step: "Never let go" }],
+    category: "Cuddle",
+    prepTime: "2 min",
+    cookTime: "Forever",
+    spiceLevel: "Medium",
+    chef: "Wife",
+    occasion: "Life",
+  },
+  {
+    id: 3,
+    //date: getDate(),
+    name: "Samosa",
+    desc: "This is the delicious meal. This is the delicious meal. This is the delicious meal. This is the delicious meal.",
+    isFav: true,
+    ingredients: [
+      { name: "Love", qty: "1sp" },
+      { name: "Effection", qty: "2sp" },
+    ],
+    method: [{ step: "Hold the hands" }, { step: "Never let go" }],
+    category: "Cuddle",
+    prepTime: "2 min",
+    cookTime: "Forever",
+    spiceLevel: "Extreme",
+    chef: "Wife",
+    occasion: "Life",
+  },
+  {
+    id: 4,
+    //date: getDate(),
+    name: "Haleem",
+    desc: "This is the delicious meal.",
+    isFav: true,
+    ingredients: [
+      { name: "Love", qty: "1sp" },
+      { name: "Effection", qty: "2sp" },
+    ],
+    method: [{ step: "Hold the hands" }, { step: "Never let go" }],
+    category: "Cuddle",
+    prepTime: "2 min",
+    cookTime: "1 hr",
+    spiceLevel: "High",
+    chef: "Wife",
+    occasion: "Life",
+  },
+]);
 </script>
 <template>
-    <section>
-        <h1>Recipes</h1>
-        <div class="card-container">
-            <RecipeCard :recipe="recipe" v-for="recipe in recipes"/>
-        </div>
-
-    </section>
+  <section>
+    <h1>Recipes</h1>
+    <div class="card-container">
+      <RecipeCard :recipe="recipe" v-for="recipe in recipes" />
+    </div>
+  </section>
 </template>
 
 <style scoped>
-.card-container{
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 }
 
 /* Media query for smaller screens */
 @media screen and (max-width: 768px) {
-  .card-container{
+  .card-container {
     grid-template-columns: repeat(2, 1fr);
   }
 }
 /* Media query for smaller screens */
 @media screen and (max-width: 580px) {
-  .card-container{
+  .card-container {
     grid-template-columns: repeat(1, 1fr);
   }
 }
