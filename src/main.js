494d8c66576b0795208415722394537eba917b23
wbajwa313 <wbajwa@choice.com.au>
2023-12-18 14:15:49 +1100

    Install Pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -25,6 +26,10 @@ import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
-app.use(router);
-app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
 app.use(Toast)
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+
